001c714d89b66628d7a1c0e5851fa0c8962a08d3
Reshmaoviya <127235929+Reshmaoviya@users.noreply.github.com>
2025-12-24 11:15:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello from ECS");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World! Deployment Successful by Reshma\n');
+});
+
+const port = 80;
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at port ${port}`);
+});
